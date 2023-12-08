e340c935f1932cc3dcfc5aacb28fedc54d31dadb
Ivankays <144226977+Ivankays@users.noreply.github.com>
2023-12-08 12:13:47 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,39 @@
+﻿program zad1;
+var
+arr: array of Integer;
+n, i, j: Integer;
+found: Boolean;
+begin
+Write('Введите количество элементов в массиве: ');
+ReadLn(n);
+SetLength(arr, n);
+WriteLn('Введите элементы массива:');
+for i := 0 to n-1 do
+begin
+ReadLn(arr[i]);
+end;
+found := False;
+for i := 0 to n-2 do
+begin
+for j := i+1 to n-1 do
+begin
+if (arr[i] * arr[j]) <= (arr[i] + arr[j]) then
+begin
+WriteLn('Найдены элементы: ', arr[i], ' и ', arr[j]);
+found := True;
+Break;
+end;
+end;
+if found then
+  begin
+    Writeln('Найдены элементы: ', arr[i], ' и ', arr[j]);
+    Writeln('Их произведение = ', arr[i] * arr[j]);
+    Writeln('Их сумма = ', arr[i] + arr[j]);
+  end
+  else
+    Writeln('Подходящая пара элементов не найдена.');
+Break;
+end;
+if not found then
+WriteLn('Таких элементов не найдено');
+end.
---
+++ 2.pas
@@ -0,0 +1,31 @@
+﻿program zad2;;
+var
+  s, sub: string;
+  i, j: integer;
+  subPos: boolean;
+begin
+  Write('Введите исходную строку: ');
+  ReadLn(s);
+  Write('Введите подстроку: ');
+  ReadLn(sub);
+  subPos := False;
+  i := 1;
+  j := 1;
+  while (i <= Length(s)) and (j <= Length(sub)) do
+  begin
+    if s[i] = sub[j] then
+    begin
+      j := j + 1;
+      if j > Length(sub) then
+      begin
+        subPos := True;
+        Break;
+      end;
+    end;
+    i := i + 1;
+  end;
+  if subPos then
+    WriteLn('Подстрока является подпоследовательностью исходной строки.')
+  else
+    WriteLn('Подстрока не является подпоследовательностью исходной строки.');
+end.
